ca5ce003d9095a53aa555df600d4d405a2b9a657
Micael Lima <micaelcomp@alu.ufc.br>
2021-12-21 23:26:25 -0300

    test: ensure CheckLastEventStatus return status done when group has no event

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -1,16 +1,19 @@
 /* eslint-disable max-classes-per-file */
 
 interface ILoadLastEventRepository {
-  loadLastEvent: (groupId: string) => Promise<void>;
+  loadLastEvent: (groupId: string) => Promise<undefined>;
 }
 
-class LoadLastEventRepositoryMock implements ILoadLastEventRepository {
+class LoadLastEventRepositorySpy implements ILoadLastEventRepository {
   groupId?: string;
   callsCount = 0;
+  output: undefined;
 
-  async loadLastEvent(groupId: string): Promise<void> {
+  async loadLastEvent(groupId: string): Promise<undefined> {
     this.groupId = groupId;
     this.callsCount += 1;
+
+    return this.output;
   }
 }
 
@@ -18,19 +21,33 @@ class CheckLastEventStatus {
   constructor(
     private readonly loadLastEventRepository: ILoadLastEventRepository,
   ) {}
-  async perform(groupId: string): Promise<void> {
+  async perform(groupId: string): Promise<string> {
     await this.loadLastEventRepository.loadLastEvent(groupId);
+
+    return 'done';
   }
 }
 
 describe('CheckLastEventStatus', () => {
   it('Should get last event data', async () => {
-    const loadLastEventRepository = new LoadLastEventRepositoryMock();
+    const loadLastEventRepository = new LoadLastEventRepositorySpy();
     const sut = new CheckLastEventStatus(loadLastEventRepository);
 
     await sut.perform('any_data');
 
     expect(loadLastEventRepository.groupId).toBe('any_data');
     expect(loadLastEventRepository.callsCount).toBe(1);
+  });
+
+  it('Should return status done when group has no event', async () => {
+    const loadLastEventRepository = new LoadLastEventRepositorySpy();
+
+    loadLastEventRepository.output = undefined;
+
+    const sut = new CheckLastEventStatus(loadLastEventRepository);
+
+    const status = await sut.perform('any_data');
+
+    expect(status).toBe('done');
   });
 });
